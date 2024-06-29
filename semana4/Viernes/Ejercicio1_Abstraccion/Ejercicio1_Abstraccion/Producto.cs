c2eb95cc750caf7b2c1362785067e88cc7ff1d19
Gabriel <gabrielsxf@gmail.com>
2024-06-28 19:51:14 -0500

    semana 4 - ejercicio1_Abstraccion

---
+++ semana4/Viernes/Ejercicio1_Abstraccion/Ejercicio1_Abstraccion/Producto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Abstraccion
+{
+    public class Producto
+    {
+        private static int contadorIds = 1;
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+
+        public Producto(string nombre, decimal precio, int stock)
+        {
+            Id = contadorIds++;
+            Nombre = nombre;
+            Precio = precio;
+            Stock = stock;
+            
+        }
+        public static decimal CalcularTotalVentas(List<Producto> productosVendidos)
+        {
+            decimal totalVentas = 0;
+            foreach(Producto i in productosVendidos)
+            {
+                totalVentas += i.Precio * i.Stock;
+            }
+            return totalVentas;
+        }
+
+        public void RestarStock(int cantidadVendida)
+        {
+            if(cantidadVendida <= Stock)
+            {
+                //Stock = Stock - cantidadVendida;
+                Stock -= cantidadVendida;
+            }
+            else
+            {
+                Console.WriteLine("No hay suficiente stock para realizar la venta");
+            }
+        }
+
+    }
+}
---
+++ semana4/Viernes/Ejercicio1_Abstraccion/Ejercicio1_Abstraccion/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using Ejercicio1_Abstraccion;
+
+public class Program
+{
+    static void Main()
+    {
+        Producto laptop = new Producto("Huawei", 2000, 10);
+        Producto celular = new Producto("Iphone", 1800, 15);
+        Producto auriculares = new Producto("Samsung", 500, 20);
+
+        List<Producto> productos = new List<Producto>();
+        laptop.RestarStock(5);
+
+        productos.Add(laptop);
+        productos.Add(celular);
+        productos.Add(auriculares);
+
+        //calcular el total de ventas
+        decimal totalVentas = Producto.CalcularTotalVentas(productos);
+        Console.WriteLine("El total de ventas es: " + totalVentas);
+    }
+}
