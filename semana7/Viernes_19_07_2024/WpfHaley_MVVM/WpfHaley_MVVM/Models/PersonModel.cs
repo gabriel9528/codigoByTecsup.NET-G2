2a9fe06eea091fa9bfbed08e4b5cb3bc1ee089f3
Gabriel <gabrielsxf@gmail.com>
2024-07-19 22:25:44 -0500

    semana 7 - viernes

---
+++ semana7/Viernes_19_07_2024/WpfHaley_MVVM/WpfHaley_MVVM/Models/PersonModel.cs
@@ -0,0 +1,47 @@
+﻿using Haley.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfHaley_MVVM.Models
+{
+    public class PersonModel : ChangeNotifier
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged(nameof(FirstName));
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                OnPropertyChanged(nameof(Age));
+            }
+        }
+
+    }
+}
---
+++ semana7/Viernes_19_07_2024/WpfHaley_MVVM/WpfHaley_MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Haley.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfHaley_MVVM.Models;
+
+namespace WpfHaley_MVVM.ViewModels
+{
+    public class MainViewModel : ChangeNotifier
+    {
+        private PersonModel _personModel;
+        public PersonModel PersonModel
+        {
+            get { return _personModel; }
+            set
+            {
+                _personModel = value;
+                OnPropertyChanged(nameof(PersonModel));
+            }
+        }
+
+        private ObservableCollection<PersonModel> _observablePersons;
+        public ObservableCollection<PersonModel> ObservablePersons
+        {
+            get { return _observablePersons; }
+            set
+            {
+                _observablePersons = value;
+
+            }
+        }
+
+        public void AddPerson()
+        {
+            ObservablePersons.Add(PersonModel);
+            PersonModel = new PersonModel();
+        }
+
+        public ICommand AddCommand => new DelegateCommand(AddPerson);
+
+        public MainViewModel()
+        {
+            //AddCommand = new AddCommand();
+            ObservablePersons = new ObservableCollection<PersonModel>();
+            PersonModel = new PersonModel();
+        }
+    }
+}
