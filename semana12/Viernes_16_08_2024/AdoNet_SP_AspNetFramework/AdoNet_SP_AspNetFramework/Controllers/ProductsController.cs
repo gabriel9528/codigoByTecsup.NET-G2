e809371854eb125c41c84c8badefa4b4d0560f41
Gabriel <gabrielsxf@gmail.com>
2024-08-16 20:33:35 -0500

    wpf-using-modelDisconnected1

---
+++ semana12/Viernes_16_08_2024/AdoNet_SP_AspNetFramework/AdoNet_SP_AspNetFramework/Controllers/ProductsController.cs
@@ -0,0 +1,92 @@
+﻿using AdoNet_SP_AspNetFramework.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdoNet_SP_AspNetFramework.Controllers
+{
+    public class ProductsController : Controller
+    {
+        Product_DAL product_DAL = new Product_DAL();
+        // GET: Products
+        public ActionResult Index()
+        {
+            var productList = product_DAL.GetAllProducts();
+            return View(productList);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ semana12/Viernes_16_08_2024/AdoNet_SP_AspNetFramework/AdoNet_SP_AspNetFramework/DAL/Product_DAL.cs
@@ -0,0 +1,49 @@
+﻿using AdoNet_SP_AspNetFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace AdoNet_SP_AspNetFramework.DAL
+{
+    public class Product_DAL
+    {
+        string connectionString = 
+            ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
+
+        //GetAllProducts
+        public List<ProductModels> GetAllProducts()
+        {
+            List<ProductModels> listProducts = new List<ProductModels>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_GetAllProducts";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dtProducts = new DataTable();
+
+                connection.Open();
+                adapter.Fill(dtProducts);
+                connection.Close();
+
+                foreach(DataRow item in dtProducts.Rows)
+                {
+                    ProductModels product = new ProductModels();
+                    product.ProductId = Convert.ToInt32(item["ProductId"]);
+                    product.ProductName = item["Product"].ToString();
+                    product.Price = Convert.ToDecimal(item["Price"]);
+                    product.Quantity = Convert.ToInt32(item["Quantity"]);
+                    product.Remarks = item["Remarks"].ToString();
+
+                    listProducts.Add(product);
+                }
+            }
+            return listProducts;
+        }
+    }
+}
