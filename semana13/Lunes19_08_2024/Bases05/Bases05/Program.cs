9ba5e040a13d1b60439d93ef4ed87ea287b30935
Gabriel <gabrielsxf@gmail.com>
2024-08-20 06:53:25 -0500

    Intro a LinQ

---
+++ semana13/Lunes19_08_2024/Bases05/Bases05/Program.cs
@@ -0,0 +1,91 @@
+﻿using System.Diagnostics.Metrics;
+using System;
+using Bases05;
+
+class Porgram
+{
+    public static void Main()
+    {
+        //Ejercicio 11: En este ejercicio, deberás trabajar con la lista de estudiantes y
+        //realizar operaciones de conteo, reversión, ordenamiento y cálculos de agregación.
+
+        //Parte 1: Contar los Estudiantes Aprobados
+        //usando LINQ para contar el número de estudiantes con un promedio(Average) mayor a 14.0.
+        //Imprime el resultado en la consola.
+
+        List<Student> studentList = new List<Student>()
+        {
+            new Student(1, "Gabriel", "Mercadotecnia", 16),
+            new Student(2, "Luis", "Derecho", 15),
+            new Student(3, "Ana", "Medicina", 17),
+            new Student(4, "Sofia", "Ingeniería", 14),
+
+        };
+
+        Console.WriteLine("*-----------------------------------------------*");
+        int queryCount =(from x in studentList
+                         where x.Average > 14
+                         select x).Count();
+
+        Console.WriteLine("Número de estudiantes con promedio mayor a 14: " + queryCount);
+
+        //Parte 2: Revertir la Lista de Estudiantes
+        //Reversa el orden de la lista de estudiantes aprobados y muestra el
+        //resultado en la consola, indicando que son "Desaprobados".
+
+        Console.WriteLine("*-----------------------------------------------*");
+        var queryReverse = (from x in studentList
+                            where x.Average > 14
+                            select x).Reverse();
+
+        foreach(var item in queryReverse)
+        {
+            Console.WriteLine(item + " Desaprobado");
+        }
+
+        //Parte 3: Ordenar la Lista de Estudiantes
+        //Ordena la lista de estudiantes aprobados por nombre y
+        //muestra el resultado en la consola.
+
+        Console.WriteLine("*-----------------------------------------------*");
+        Console.WriteLine("Descendente");
+        var queryOrderingByDescending = from x in studentList
+                                        orderby x.Average descending
+                                        select x;
+
+        foreach(var item in queryOrderingByDescending)
+        {
+            Console.WriteLine(item);
+        }
+
+        Console.WriteLine("*-----------------------------------------------*");
+        Console.WriteLine("Ascendente");
+        var queryOrderingByAscending = from x in studentList
+                                        orderby x.Average ascending
+                                        select x;
+
+        foreach (var item in queryOrderingByAscending)
+        {
+            Console.WriteLine(item);
+        }
+
+        int[] numbers = { 2, 8, 25, 16, 31, 5, 34, 24, 11 };
+
+        //Obten el minimo valor
+        int min = numbers.Min();
+        Console.WriteLine("Minimo: " + min);
+
+        //Obten el maximo valor
+        int max = (from x in numbers select x).Max();
+        Console.WriteLine("Maximo: " + max);
+
+        //Obten el promedio
+        double average = numbers.Average();
+        Console.WriteLine("Promedio: " + average);
+
+        //Obten la suma
+        int sum = (from x in numbers select x).Sum();
+        Console.WriteLine("Suma: " + sum);
+
+    }
+}
---
+++ semana13/Lunes19_08_2024/TiposDeObjetps/TiposDeObjetps/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using TiposDeObjetps;
+
+class Program
+{
+    public static void Main()
+    {
+        //Ejercicio 10
+        //Uso de coleccione genericas: donde vamos a crear una coleccion generica e
+        //identificar un unico tipo de objeto
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.AddRange(new object[]
+        {
+            "hello world",
+            5,
+            2.99,
+            true,
+            10,
+            18,
+            8.58,
+            "goodbye world"
+        });
+
+        var values = arrayList.OfType<double>();
+        foreach(var value in values)
+        {
+            Console.WriteLine(value);
+        }
+
+
+        Console.WriteLine("*--------------------------------*");
+        Console.WriteLine("Ejercicio 2A");
+        ArrayList arrayList2 = new ArrayList();
+        arrayList2.AddRange(new object[]
+        {
+            new Student(1, "Gabriel", "Mercadotecnia", 16),
+            new Student(2, "Luis", "Derecho", 15),
+            new Student(3, "Ana", "Medicina", 17),
+            new Student(4, "Sofia", "Ingeniería", 14),
+
+        });
+
+        var values2 = arrayList2.OfType<Student>();
+        foreach (var value in values2)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
