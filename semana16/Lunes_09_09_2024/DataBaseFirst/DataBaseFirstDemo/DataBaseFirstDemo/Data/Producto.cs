fc9746399930dfb99c21c9bfa62f1a6dfc97541a
Gabriel <gabrielsxf@gmail.com>
2024-09-09 23:14:05 -0500

    lunes

---
+++ semana16/Lunes_09_09_2024/DataBaseFirst/DataBaseFirstDemo/DataBaseFirstDemo/Data/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseFirstDemo.Data;
+
+[Table("PRODUCTO")]
+public partial class Producto
+{
+    [Key]
+    public int IdProducto { get; set; }
+
+    public Guid CodProducto { get; set; }
+
+    [StringLength(200)]
+    [Unicode(false)]
+    public string NombreProducto { get; set; } = null!;
+
+    [Column("PVP")]
+    public double Pvp { get; set; }
+
+    public double PrecioCompra { get; set; }
+
+    public bool Tangible { get; set; }
+
+    public double IvaActual { get; set; }
+
+    public int StockDisponible { get; set; }
+}
---
+++ semana16/Lunes_09_09_2024/DataBaseFirst/DataBaseFirstDemo/DataBaseFirstDemo/Data/Prueba1Context.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseFirstDemo.Data;
+
+public partial class Prueba1Context : DbContext
+{
+    public Prueba1Context()
+    {
+    }
+
+    public Prueba1Context(DbContextOptions<Prueba1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
