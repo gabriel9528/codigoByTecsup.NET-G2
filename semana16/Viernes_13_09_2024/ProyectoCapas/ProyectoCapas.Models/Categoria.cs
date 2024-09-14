f517332a62f67866532f8433213b8b93a95bb4d1
Gabriel <gabrielsxf@gmail.com>
2024-09-14 16:08:35 -0500

    semana 16

---
+++ semana16/Viernes_13_09_2024/ProyectoCapas/ProyectoCapas.Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCapas.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio un nombre para la Categoria")]
+        [Display(Name = "Nombre de la Categoria")]
+        public string? Nombre { get; set; }
+
+        [Display(Name = "Orden de la Categoria")]
+        [Range(1, 100, ErrorMessage = "El orden de la Categoria debe estar entre 1 y 100")]
+        public int? Orden { get; set; }
+    }
+}
