bea78e473bfe9a8aad7f37526b98118d5ea0fb53
Gabriel <gabrielsxf@gmail.com>
2024-09-18 21:49:21 -0500

    semana 17

--- semana17/Miercoles_18_09_2024/ProyectoCapas/ProyectoCapas/Areas/Admin/Controllers/SlidersController.cs
+++ semana17/Miercoles_18_09_2024/ProyectoCapas/ProyectoCapas/Areas/Admin/Controllers/SlidersController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProyectoCapas.AccesoDatos.Data.Repository.IRepository;
+using ProyectoCapas.Models;
+using System.Net.WebSockets;
 
 namespace ProyectoCapas.Areas.Admin.Controllers
 {
@@ -17,6 +19,58 @@ namespace ProyectoCapas.Areas.Admin.Controllers
         {
             return View();
         }
+
+        #region Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Slider slider)
+        {
+            string rutaPrincipal = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+            if (files.Count() > 0)
+            {
+                string nombreArchivo = Guid.NewGuid().ToString();
+                var upload = Path.Combine(rutaPrincipal, @"imagenes\sliders");
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var fileStream = new FileStream(Path.Combine(upload, nombreArchivo + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(fileStream);
+                }
+
+                slider.UrlImagen = @"\imagenes\sliders\" + nombreArchivo + extension;
+
+                _icontenedorTrabajo.ISliderRepository.Add(slider);
+                _icontenedorTrabajo.Save();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(slider);
+        }
+        #endregion
+
+
+        #region Edit
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if(id != null)
+            {
+                Slider slider = _icontenedorTrabajo.ISliderRepository.GetById(id.GetValueOrDefault());
+                return View(slider);
+            }
+            return NotFound();
+        }
+
+        #endregion
 
         #region LLAMADAS A LA API
 
