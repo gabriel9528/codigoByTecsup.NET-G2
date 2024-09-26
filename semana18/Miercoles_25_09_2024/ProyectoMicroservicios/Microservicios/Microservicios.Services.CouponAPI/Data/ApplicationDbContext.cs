faea9f33b391027a4560dc4763ca0580b48407ff
Gabriel <gabrielsxf@gmail.com>
2024-09-25 19:53:49 -0500

    semana 18

---
+++ semana18/Miercoles_25_09_2024/ProyectoMicroservicios/Microservicios/Microservicios.Services.CouponAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microservicios.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Microservicios.Services.CouponAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupon { get; set; }
+    }
+}
