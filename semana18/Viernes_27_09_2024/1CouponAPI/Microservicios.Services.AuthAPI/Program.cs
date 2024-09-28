0ee89f06681ef8ae01d0fddcef4d42efac4b9149
Gabriel <gabrielsxf@gmail.com>
2024-09-27 22:20:27 -0500

    semana 18

--- semana18/Viernes_27_09_2024/1CouponAPI/Microservicios.Services.AuthAPI/Program.cs
+++ semana18/Viernes_27_09_2024/1CouponAPI/Microservicios.Services.AuthAPI/Program.cs
@@ -2,6 +2,7 @@ using Microservicios.Services.AuthAPI.Data;
 using Microservicios.Services.AuthAPI.Models;
 using Microservicios.Services.AuthAPI.Services;
 using Microservicios.Services.AuthAPI.Services.IServices;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,11 +16,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("JwtOptions"));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 
 var app = builder.Build();
 
