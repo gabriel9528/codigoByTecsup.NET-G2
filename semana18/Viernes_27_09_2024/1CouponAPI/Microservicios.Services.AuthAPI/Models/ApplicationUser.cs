52cb50019d4b1373c8076f8a58878fac93d37d53
Gabriel <gabrielsxf@gmail.com>
2024-09-27 19:45:28 -0500

    semana 18 viernes

---
+++ semana18/Viernes_27_09_2024/1CouponAPI/Microservicios.Services.AuthAPI/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Microservicios.Services.AuthAPI.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Nombres { get; set; }
+    }
+}
---
+++ semana18/Viernes_27_09_2024/1CouponAPI/Microservicios.Services.AuthAPI/Program.cs
@@ -0,0 +1,42 @@
+using Microservicios.Services.AuthAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+ApplyMigration();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
